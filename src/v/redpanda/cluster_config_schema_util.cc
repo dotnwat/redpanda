572f34053efb353c1a329988d324031feaa741e4
Noah Watkins <noahwatkins@gmail.com>
2022-08-16 12:50:47 -0700

    redpanda: fixup header usage
    Only include the generated file where it's needed in .cc translation
    units rather than in header. This is useful because the header involves
    a generated file and that influences how the dependencies are setup so
    that that file is available.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/v/redpanda/cluster_config_schema_util.cc
+++ src/v/redpanda/cluster_config_schema_util.cc
@@ -9,6 +9,7 @@
  * by the Apache License, Version 2.0
  */
 #include "cluster_config_schema_util.h"
+#include "redpanda/admin/api-doc/cluster_config.json.h"
 
 // This is factored out to make it a separate binary that can generate schema
 // without bringing up a redpanda cluster. Down stream tools can make use of
