5cef657e1916b8fdd5bd7765696ce89e797cfdbd
Noah Watkins <noahwatkins@gmail.com>
2025-07-03 17:45:34 +0000

    epoch wip
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ src/v/cloud_topics/ephemeral_cluster_services.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2025 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+#pragma once
+
+#include "cloud_topics/cluster_services.h"
+
+#include <seastar/core/coroutine.hh>
+
+namespace experimental::cloud_topics {
+
+class ephemeral_cluster_services : public cluster_services {
+public:
+    seastar::future<epoch_type> current_epoch() override { co_return epoch_++; }
+
+private:
+    epoch_type epoch_{0};
+};
+
+} // namespace experimental::cloud_topics
